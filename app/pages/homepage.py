bb6f32ed06fa3899d9ccf6b5ee41387f194b0eaa
Emily Roullier-Anderson <emily.tara.anderson@gmail.com>
2024-07-02 11:17:44 +0200

    Final first version

--- app/pages/homepage.py
+++ app/pages/homepage.py
@@ -1,10 +1,20 @@
 import streamlit as st
 
+
+t.write('# This is the homepage')
+
 st.set_page_config(page_title="HR Data analytics")
+
 st.markdown(
     """ # HR Data analytics
+    ## Analysing HR data to predict and improve employee attrition rates.
+    What is the probability that your current employee will leave the company, and what are the main factors due to influence this decision ?
     """
+
 )
+
+url = "https://netchex.com/wp-content/uploads/2022/12/HR-Analytics.png"
+st.image(url)
 
 homepage = st.Page("pages/homepage.py", title="Homepage")
 EDA_page = st.Page("pages/EDA_page.py", title="EDA")
