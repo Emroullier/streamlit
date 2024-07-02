6fa843a8f062bbf55dc2b004b52c99e079f02128
Emily Roullier-Anderson <emily.tara.anderson@gmail.com>
2024-07-02 13:08:13 +0200

    Edited navigator 2

--- app/pages/hiring_page.py
+++ app/pages/hiring_page.py
@@ -8,45 +8,41 @@ def app():
     st.title("Hiring Page")
     st.write("Welcome to the Hiring Page!")
 
-st.markdown(
-    """ # Support to HR decisions in new hiring
-    ## Based on job opening requirements and applicant's information, which applicant is more likely to stay in the company ?
+    st.markdown(
+        """
+        # Support to HR decisions in new hiring
+        ## Based on job opening requirements and applicant's information, which applicant is more likely to stay in the company?
+        """
+    )
 
-    """
-)
-# ************************** Connection to API ***************************************
-uploaded_file = st.file_uploader("Upload a json file")
+    # ************************** Connection to API ***************************************
+    uploaded_file = st.file_uploader("Upload a JSON file")
 
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
 
+        json_data = json.loads(bytes_data)
+        X = pd.DataFrame(json_data)
+        hiring_features_cols = ['Department','GEO','Role',
+                                'average_montly_hours','salary',
+                                'Will_Relocate','Gender']
 
-    json_data = json.loads(bytes_data)
-    X = pd.DataFrame(json_data)
-    hiring_features_cols = ['Department','GEO','Role',
-                            'average_montly_hours','salary',
-                            'Will_Relocate','Gender']
+        X = X[hiring_features_cols]
 
-    X = X[hiring_features_cols]
+        st.write(f"## Description of candidates")
+        st.write(X)
 
-    st.write(f"## Description of candidates")
-    st.write(X)
+        # API endpoint
+        URL = "https://hrdataanalytics-4bdr2jy2qa-od.a.run.app/upload_predict_hiring"
 
-    # api-endpoint
-    URL = "https://hrdataanalytics-4bdr2jy2qa-od.a.run.app/upload_predict_hiring"
+        # Sending POST request and saving the response as a response object
+        r = requests.post(url=URL, files={'upload_file': bytes_data})
 
-    # defining a params dict for the parameters to be sent to the API
-    data = bytes_data
-
-    # sending get request and saving the response as response object
-    r = requests.post(url = URL, files = {'upload_file': data})
-
-    st.write(f"## Ranking of applicants based of probability to stay with the company:")
-    ranking = json.loads(r.text)
-    ranking_df = pd.DataFrame(ranking['Ranking'])
-    st.write(ranking_df)
-    # **************************************************************************************
-
-else:
-    st.write("Waiting for input...")
+        st.write(f"## Ranking of applicants based on probability to stay with the company:")
+        ranking = json.loads(r.text)
+        ranking_df = pd.DataFrame(ranking['Ranking'])
+        st.write(ranking_df)
+    else:
+        st.write("Waiting for input...")
+Update app.py
