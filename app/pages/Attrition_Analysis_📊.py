5bec42a2023359fc434f41dea0e772e448bcfb3a
Louis Gokelaere <louis.gokelaere@gmail.com>
2024-07-03 11:40:18 +0200

    Modified EDA page to Attrition Rate, and added dummy text

---
+++ app/pages/Attrition_Analysis_📊.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+st.title("Attrition Rate Analysis")
+st.markdown(
+    """
+    ## Why are your employees leaving your company?
+
+    #### 1) 5 Most Important Factors Resulting In Current Attrition Rate
+
+        1. Salary
+
+        2. Workload
+
+        3. Example
+
+        4. Example
+
+        5. Example
+
+    #### 2) Employee Profiling
+
+
+    """
+)
--- app/pages/EDA_page_📊.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-
-st.title("EDA Page")
-st.write("Welcome to the EDA Page!")
