5029097f5f0503d60e5b847f2a314505da98a88f
Sarah Diethelm <sarahdiethelm24@gmail.com>
2024-07-03 12:53:09 +0200

    Centered title and sub title, also made writing all the same size

--- app/pages/Hiring_Page_📨.py
+++ app/pages/Hiring_Page_📨.py
@@ -2,21 +2,35 @@ import streamlit as st
 import requests
 import pandas as pd
 import json
-# from io import StringIO
-
-#############################
-# Removed title and subtitle
-#############################
 
 st.markdown(
     """
-    # Support to HR decisions in new hiring
-    ## Based on job opening requirements and applicant's information, which applicant is more likely to stay in the company?
-    """
+    <style>
+    .title {
+        text-align: center;
+    }
+    .subtitle {
+        text-align: center;
+        color: gray;
+        font-size: 1.5em;
+    }
+    .first-sentence {
+        text-align: left;
+        margin-top: 20px;
+        font-size: 1.2em;
+    }
+    </style>
+    <h1 class="title">Support HR Decisions in New Hiring</h1>
+    <h2 class="subtitle">Based on the job description and an applicant's information,
+    which applicant is more likely to be retained within the company?</h2>
+    """,
+    unsafe_allow_html=True
 )
 
 # ************************** Connection to API ***************************************
-uploaded_file = st.file_uploader("Upload a JSON file")
+st.markdown('<p class="header-text no-space">Please upload the JSON file below</p>', unsafe_allow_html=True)
+
+uploaded_file = st.file_uploader("")
 
 if uploaded_file is not None:
     # To read file as bytes:
@@ -30,7 +44,7 @@ if uploaded_file is not None:
 
     X = X[hiring_features_cols]
 
-    st.write(f"## Description of candidates")
+    st.write(f"## Description of applicants")
     st.write(X)
 
     # API endpoint
@@ -39,9 +53,9 @@ if uploaded_file is not None:
     # Sending POST request and saving the response as a response object
     r = requests.post(url=URL, files={'upload_file': bytes_data})
 
-    st.write(f"## Ranking of applicants based on probability to stay with the company:")
+    st.write(f"## Ranking of applicants based on probability of retention")
     ranking = json.loads(r.text)
     ranking_df = pd.DataFrame(ranking['Ranking'])
     st.write(ranking_df)
 else:
-    st.write("Waiting for input...")
+    st.markdown('<p class="header-text">No File Uploaded</p>', unsafe_allow_html=True)
