0930b1220ce9082ff4407ae290f2a23f3c9e2031
Thierry CAROLE <thcarole1@hotmail.com>
2024-07-05 10:32:25 +0200

    Interactive plots

--- app/pages/Attrition_Analysis_📊.py
+++ app/pages/Attrition_Analysis_📊.py
@@ -50,103 +50,58 @@ markdown_2="""
     </div>
     """
 
-
-st.markdown(markdown_1, unsafe_allow_html=True)
-
-plot = st.radio(
-    "Choose your graph",
-    ["Feature importance", "Time spent in the company",
-     "Salary", "Number of projects", "Step counts"],
-    captions = ["Short explanation", "Short explanation",
-                "Short explanation", "Short explanation",
-                "Short explanation"])
-
-if plot == "Feature importance":
-    # ************************** Connection to API : Feature importance ***************************************
-    # api-endpoint
-    URL = "https://hrdataanalytics-4bdr2jy2qa-od.a.run.app/plot_feature_importance"
-
-    # sending get request and saving the response as response object
-    r = requests.post(url = URL)
-
-    st.image(r.content,
-                caption=None,
-                width=None,
-                use_column_width=None,
-                clamp=False,
-                channels="RGB",
-                output_format="PNG")
-    # **************************************************************************************
-
-if plot == "Time spent in the company":
-        # ************************** Connection to API : Time spent in the company ***************************************
-    # api-endpoint
-    URL = "https://hrdataanalytics-4bdr2jy2qa-od.a.run.app/plot_time_spend_company"
-
-    # sending get request and saving the response as response object
-    r = requests.post(url = URL)
-
-    st.image(r.content,
-                caption=None,
-                width=None,
-                use_column_width=None,
-                clamp=False,
-                channels="RGB",
-                output_format="PNG")
-    # **************************************************************************************
-
-if plot == "Salary":
-    # ************************** Connection to API  : Salary ***************************************
-    # api-endpoint
-    URL = "https://hrdataanalytics-4bdr2jy2qa-od.a.run.app/plot_salary"
-
-    # sending get request and saving the response as response object
-    r = requests.post(url = URL)
-
-    st.image(r.content,
-                caption=None,
-                width=None,
-                use_column_width=None,
-                clamp=False,
-                channels="RGB",
-                output_format="PNG")
-    # **************************************************************************************
-
-
-if plot == "Number of projects":
-    # ************************** Connection to API :  Number of projects ***************************************
-    # api-endpoint
-    URL = "https://hrdataanalytics-4bdr2jy2qa-od.a.run.app/plot_number_projects"
-
-    # sending get request and saving the response as response object
-    r = requests.post(url = URL)
-
-    st.image(r.content,
-                caption=None,
-                width=None,
-                use_column_width=None,
-                clamp=False,
-                channels="RGB",
-                output_format="PNG")
-    # **************************************************************************************
-
-
-if plot == "Remote work percentage":
-    # ************************** Connection to API ***************************************
-    # api-endpoint
-    URL = "https://hrdataanalytics-4bdr2jy2qa-od.a.run.app/plot_step_count"
-
-    # sending get request and saving the response as response object
-    # r = requests.post(url = URL, params = {'feature' : col})
-    r = requests.post(url = URL)
-
-    st.image(r.content,
-                caption=None,
-                width=None,
-                use_column_width=None,
-                clamp=False,
-                channels="RGB",
-                output_format="PNG")
-    # **************************************************************************************
-
 st.markdown(markdown_2, unsafe_allow_html=True)
+
+st.write("----------------------------")
+
+#Choose the kind of plot
+option_plot = st.selectbox(
+    "**What type of plot ?**",
+    ('violin', 'crosstab', 'hist'),
+    key = 'option_plot'
+    )
+
+if option_plot == 'violin':
+    endpoint = 'plot_violin'
+    selection = ('average_montly_hours',
+                'LinkedIn_Hits',
+                'Sensor_StepCount',
+                'Sensor_Heartbeat(Average/Min)',
+                'Sensor_Proximity(1-highest/10-lowest)',
+                'last_evaluation')
+
+if option_plot == 'crosstab':
+    endpoint = 'plot_crosstab'
+    selection = ('Gender',
+                'promotion_last_5years',
+                'Work_accident',
+                'Rising_Star',
+                'Percent_Remote',
+                'number_project',
+                'salary')
+
+if option_plot == 'hist':
+    endpoint = 'plot_freq'
+    selection = ('time_spend_company', 'Rising_Star')
+
+option_final = st.selectbox(
+    "**Which feature do you want to plot ?**",
+    selection,
+    key='option_final'
+    )
+
+# ************************** Connection to API : Violin plots ***************
+# api-endpoint
+URL = f"https://hrdataanalytics-4bdr2jy2qa-od.a.run.app/{endpoint}"
+params = {'input' : option_final}
+# sending get request and saving the response as response object
+r = requests.post(url = URL, params = params)
+
+st.image(r.content,
+            caption=None,
+            width=None,
+            use_column_width=None,
+            clamp=False,
+            channels="RGB",
+            output_format="PNG")
+# ***************************************************************************
